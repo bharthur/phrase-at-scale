86a975f5b602f309f7008deb70f4340ade9dc318
Shiv Bharthur <shiv.bharthur@hotmail.com>
2018-07-15 17:04:43 -0400

    job-title

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 
 # these are settings for generating phrases and annotating corpora with phrases
 phrase_generator = \
-    {'input-file': 'data/raw-data/reviews_data.txt.gz',  # data files, can be file or folder
+    {'input-file': 'data/raw-data/job_title.txt.gz',  # data files, can be file or folder
      'output-folder': 'data/tagged-data/',  # directory to output tagged corpora
      # where to store the phrase file? (only needed if dotag=True)
-     'phrase-file': 'top-opinrank-phrases.txt',
+     'phrase-file': 'job-title-phrases.txt',
      'dotag': True,  # do you want to tag your corpora with phrases and generate a new version of it?
      # a list of stop words in your language
      'stop-file': 'stopwords-en.txt',
@@ -13,4 +13,3 @@ phrase_generator = \
      # sized dataset
      'min-phrase-count': 60
      }
-
